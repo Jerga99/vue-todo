7e9529ee09a20e06b49d17e61c3a3d0c90a68beb
Filip Jerga <jerga99@gmail.com>
2019-08-01 22:14:55 +0200

    4 Functions Part 1

--- src/playground.js
+++ src/playground.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable */
 
 
 
@@ -6,18 +6,50 @@
 export default function() {
   console.log('Hello World!!!!!!')
 
-  var firstName = "Filip" // string
-  var age = 28 // number
-  var isMale = true // boolean
-  var dontKnow = null // unknow value
-  var a
+  // 2 VARIABLES
+  // var firstName = "Filip" // string
+  // var age = 28 // number
+  // var isMale = true // boolean
+  // var dontKnow = null // unknow value
+  // var a
+
+  // var person = {
+  //   name: 'Filip',
+  //   lastName: 'Jerga'
+  // }
+
+  // Create all types of variables of your choice
+  // Check their types with "typeof"
+  // 2 VARIABLES
 
   var person = {
     name: 'Filip',
     lastName: 'Jerga'
   }
 
-  // Create all types of variables of your choice
-  // Check their types with "typeof"
+  var person1 = {
+    name: 'John',
+    lastName: 'Snow'
+  }
+
+  function printUserInfo(user) {
+    debugger
+    console.log('Consoling log out person informations!')
+    console.log('Hello ' + user.name + ' ' + user.lastName)
+  }
+
+  printUserInfo(person)
+  printUserInfo(person1)
+
 
 }
+
+
+
+
+
+
+
+
+
+
