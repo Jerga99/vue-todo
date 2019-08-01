e69719dcafc38dcd370d56711d6ebbb9e6ae32e9
Filip Jerga <jerga99@gmail.com>
2019-08-01 21:26:29 +0200

    3 About Variables

--- src/playground.js
+++ src/playground.js
@@ -5,4 +5,19 @@
 
 export default function() {
   console.log('Hello World!!!!!!')
+
+  var firstName = "Filip" // string
+  var age = 28 // number
+  var isMale = true // boolean
+  var dontKnow = null // unknow value
+  var a
+
+  var person = {
+    name: 'Filip',
+    lastName: 'Jerga'
+  }
+
+  // Create all types of variables of your choice
+  // Check their types with "typeof"
+
 }
