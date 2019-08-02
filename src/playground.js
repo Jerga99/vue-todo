788af02a0b097863ebac9424cceb2f736cb7dee0
Filip Jerga <jerga99@gmail.com>
2019-08-02 21:55:02 +0200

    6-8 Operators

--- src/playground.js
+++ src/playground.js
@@ -3,86 +3,85 @@
 
 
 
-export default function() {
-  // console.log('Hello World!!!!!!')
+// export default function() {
+//   // console.log('Hello World!!!!!!')
 
-  // 2 VARIABLES
-  // var firstName = "Filip" // string
-  // var age = 28 // number
-  // var isMale = true // boolean
-  // var dontKnow = null // unknow value
-  // var a
+//   // 2 VARIABLES
+//   // var firstName = "Filip" // string
+//   // var age = 28 // number
+//   // var isMale = true // boolean
+//   // var dontKnow = null // unknow value
+//   // var a
 
-  // var person = {
-  //   name: 'Filip',
-  //   lastName: 'Jerga'
-  // }
+//   // var person = {
+//   //   name: 'Filip',
+//   //   lastName: 'Jerga'
+//   // }
 
-  // Create all types of variables of your choice
-  // Check their types with "typeof"
-  // 2 VARIABLES
+//   // Create all types of variables of your choice
+//   // Check their types with "typeof"
+//   // 2 VARIABLES
 
-  // var person = {
-  //   name: 'Filip',
-  //   lastName: 'Jerga'
-  // }
+//   // var person = {
+//   //   name: 'Filip',
+//   //   lastName: 'Jerga'
+//   // }
 
-  // var person1 = {
-  //   name: 'John',
-  //   lastName: 'Snow'
-  // }
+//   // var person1 = {
+//   //   name: 'John',
+//   //   lastName: 'Snow'
+//   // }
 
-  // function printUserInfo(user) {
-  //   debugger
-  //   console.log('Consoling log out person informations!')
-  //   console.log('Hello ' + user.name + ' ' + user.lastName)
-  // }
+//   // function printUserInfo(user) {
+//   //   debugger
+//   //   console.log('Consoling log out person informations!')
+//   //   console.log('Hello ' + user.name + ' ' + user.lastName)
+//   // }
 
-  // printUserInfo(person)
-  // printUserInfo(person1)
+//   // printUserInfo(person)
+//   // printUserInfo(person1)
 
-  // var hours = 23
-  // var seconds = hours * 60 * 60
-  // console.log(seconds)
-
-
-  function transformHoursToSeconds(hours) {
-    return hours * 60 * 60
-  }
-
-  // var result = transformHoursToSeconds(19)
-  // console.log(result)
-
-  function printSomething(something) {
-    console.log(something)
-  }
-
-  // 36000
-  printSomething(transformHoursToSeconds(10))
-  // Annonymous functon
-
-  // Hello world
-  // Hello Filip, Good job with coding!
-  printSomething((function() {
-    console.log('Hello world')
-    return 'Hello Filip, Good job with coding!'
-  }()))
-
-  // {a: 1, b: 2}
-  printSomething({a: 1, b: 2})
+//   // var hours = 23
+//   // var seconds = hours * 60 * 60
+//   // console.log(seconds)
 
 
-  function countPersonBirthYear(personAge) {
-     var currentYear = 2019
-     var birthYear = currentYear - personAge
-     return birthYear
-  }
+//   function transformHoursToSeconds(hours) {
+//     return hours * 60 * 60
+//   }
 
-  var resolvedYear = countPersonBirthYear(33)
-  console.log(resolvedYear)
+//   // var result = transformHoursToSeconds(19)
+//   // console.log(result)
 
-}
+//   function printSomething(something) {
+//     console.log(something)
+//   }
 
+//   // 36000
+//   printSomething(transformHoursToSeconds(10))
+//   // Annonymous functon
+
+//   // Hello world
+//   // Hello Filip, Good job with coding!
+//   printSomething((function() {
+//     console.log('Hello world')
+//     return 'Hello Filip, Good job with coding!'
+//   }()))
+
+//   // {a: 1, b: 2}
+//   printSomething({a: 1, b: 2})
+
+
+//   function countPersonBirthYear(personAge) {
+//      var currentYear = 2019
+//      var birthYear = currentYear - personAge
+//      return birthYear
+//   }
+
+//   var resolvedYear = countPersonBirthYear(33)
+//   console.log(resolvedYear)
+
+// }
 
 
 // TODO: Create function called "countPersonBirthYear"
@@ -92,6 +91,45 @@ export default function() {
 // call function "countPersonBirthYear" and assign returned value to
 // variable called "resolvedYear"
 // console.log out resolvedYear
+
+
+
+export default function() {
+
+
+  var result = 7 + 1
+  var result2 = 7 / 1
+  var result3 = 7 % 2
+  var result4 = 7 - 1
+  var result5 = 7 / 0
+  var result6 = 7 * 0
+
+  var b = 9
+  var result7 = ++b
+  var a = 7
+  var result8 = --a
+  var result9 = "Filip" + "Jerga"
+  var result10 = "1" + 2
+  var result11 = 2 + "1"
+  var result12 = 3 + 3 + "1"
+  var result13 = 7 - "2"
+  var result14 = "8" / "4"
+  var result15 = 2 ** 3
+
+  debugger
+  result13 -= 2
+  result15 += 7
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
