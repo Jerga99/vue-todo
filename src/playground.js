de3dc98a01baacb4af27045f93ce29a47f7745aa
Filip Jerga <jerga99@gmail.com>
2019-08-03 23:08:43 +0200

    18 Arrays Introduction

--- src/playground.js
+++ src/playground.js
@@ -402,87 +402,106 @@ export default function() {
   // }
 
 
-  const dog = {
-    breed: 'German Shepard',
-    age: 5,
-    children: 3,
-    isAfterInjury: true,
-    vacination: {
-      vacination1: true,
-      vacination2: false
-    }
-  }
+  // const dog = {
+  //   breed: 'German Shepard',
+  //   age: 5,
+  //   children: 3,
+  //   isAfterInjury: true,
+  //   vacination: {
+  //     vacination1: true,
+  //     vacination2: false
+  //   }
+  // }
 
-  if (dog.breed === 'German Shepard' && dog.age > 3) {
-    console.log('Waf Waf!')
-  } else {
-    console.log('Dog is sad :(')
-  }
+  // if (dog.breed === 'German Shepard' && dog.age > 3) {
+  //   console.log('Waf Waf!')
+  // } else {
+  //   console.log('Dog is sad :(')
+  // }
 
-  if (dog.children <= 3 && dog.isAfterInjury === false) {
-    console.log('Waf Waf!')
-  } else {
-    console.log('Dog is sad :(')
-  }
+  // if (dog.children <= 3 && dog.isAfterInjury === false) {
+  //   console.log('Waf Waf!')
+  // } else {
+  //   console.log('Dog is sad :(')
+  // }
 
-    if (dog.vacination.vacination1) {
-    console.log('Waf Waf!')
-  } else {
-    console.log('Dog is sad :(')
-  }
+  //   if (dog.vacination.vacination1) {
+  //   console.log('Waf Waf!')
+  // } else {
+  //   console.log('Dog is sad :(')
+  // }
 
-  if ((dog.children > 2 || dog.vacination.vacination1 === true) && dog.vacination.vacination2 === true) {
-    console.log('Waf Waf!')
-  } else {
-    console.log('Dog is sad :(')
-  }
+  // if ((dog.children > 2 || dog.vacination.vacination1 === true) && dog.vacination.vacination2 === true) {
+  //   console.log('Waf Waf!')
+  // } else {
+  //   console.log('Dog is sad :(')
+  // }
 
-  // Tricky one! Why is "!" in front of expresion ?
-  if (!dog.isAfterInjury) {
-    console.log('Waf Waf!')
-  } else {
-    console.log('Dog is sad :(')
-  }
-
-
-  // Strict operators
-  if ( 2 == "2") {
-    console.log('Whaaaat ?')
-  } else {
-    console.log('Hmmmmm :(')
-  }
-
-  if ( 2 === "2") {
-    console.log('Whaaaat ?')
-  } else {
-    console.log('Hmmmmm :(')
-  }
-
-  if ( 1 === true) {
-    console.log('Whaaaat ?')
-  } else {
-    console.log('Hmmmmm :(')
-  }
+  // // Tricky one! Why is "!" in front of expresion ?
+  // if (!dog.isAfterInjury) {
+  //   console.log('Waf Waf!')
+  // } else {
+  //   console.log('Dog is sad :(')
+  // }
 
 
-  if ( 1 == true) {
-    console.log('Whaaaat ?')
-  } else {
-    console.log('Hmmmmm :(')
-  }
+  // // Strict operators
+  // if ( 2 == "2") {
+  //   console.log('Whaaaat ?')
+  // } else {
+  //   console.log('Hmmmmm :(')
+  // }
+
+  // if ( 2 === "2") {
+  //   console.log('Whaaaat ?')
+  // } else {
+  //   console.log('Hmmmmm :(')
+  // }
+
+  // if ( 1 === true) {
+  //   console.log('Whaaaat ?')
+  // } else {
+  //   console.log('Hmmmmm :(')
+  // }
 
 
-  if ( 0 === false) {
-    console.log('Whaaaat ?')
-  } else {
-    console.log('Hmmmmm :(')
-  }
+  // if ( 1 == true) {
+  //   console.log('Whaaaat ?')
+  // } else {
+  //   console.log('Hmmmmm :(')
+  // }
 
-   if ( 0 == false) {
-    console.log('Whaaaat ?')
-  } else {
-    console.log('Hmmmmm :(')
-  }
+
+  // if ( 0 === false) {
+  //   console.log('Whaaaat ?')
+  // } else {
+  //   console.log('Hmmmmm :(')
+  // }
+
+  //  if ( 0 == false) {
+  //   console.log('Whaaaat ?')
+  // } else {
+  //   console.log('Hmmmmm :(')
+  // }
+
+
+  const personList = [
+    {firstName: 'Filip', lastName: 'Jerga'},
+    {firstName: 'John', lastName: 'Snow'},
+    {firstName: 'Filip', lastName: 'Smith'},
+    {firstName: 'Emma', lastName: 'Green'}
+  ]
+
+  // for (let i = 0; i < personList.length; i++) {
+  //   console.log(personList[i].firstName)
+  // }
+
+  personList.forEach(function(person, index) {
+    console.log(index)
+    console.log(person.firstName)
+  })
+
+  personList.forEach(person => console.log(person.firstName))
 
 
 
