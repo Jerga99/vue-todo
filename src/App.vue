54d9fd4d7ada21897259fbe92edf44d8f07145af
Filip Jerga <jerga99@gmail.com>
2019-08-04 11:25:56 +0200

    24 Data and Methods

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,61 @@
+
+<!-- HTML part -->
 <template>
   <div id="app">
     <h1>Hello World</h1>
+    <div>{{text}}</div>
+    <div>{{someNumber}}</div>
+    <div>{{canDecide}}</div>
+    <div>{{someObject}}</div>
+    <div>{{someObject.a}}</div>
+    <div>{{someObject.c}}</div>
+    <div>{{dataFunction}}</div>
+    <div>{{dataFunction()}}</div>
+    <div>{{testFunctionA()}}</div>
   </div>
 </template>
 
+<!-- JS part -->
 <script>
 import playground from './playground'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      text: 'Some String value',
+      someNumber: 16,
+      canDecide: true,
+      list: [1,2,3],
+      someObject: {
+        a: 1,
+        b: 2,
+        c: [1,2,3,4,5]
+      },
+      dataFunction() {
+        console.log('Data function')
+        return 'I am Data function'
+      }
+    }
+  },
   // This function is run automaticaly by VUE framework
   created() {
     playground()
+  },
+  methods: {
+    testFunctionA() {
+      console.log(this.text)
+      console.log(this.someNumber)
+      console.log(this.someObject.a)
+      this.dataFunction()
+
+      return 'I a test function A'
+    }
   }
 }
 </script>
 
+<!-- Stylings, css, scss.... -->
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
