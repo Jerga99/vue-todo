3bbaac97f8f780d300ff7e3ca4ffe46c93e2a246
Filip Jerga <jerga99@gmail.com>
2019-08-04 20:44:00 +0200

    29 Props

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -5,14 +5,31 @@
   <div class="todo-item">
     <div class="todo-item-content">
       <div class="todo-item-content-title">
-        Walk the dog
+        {{title}}
       </div>
       <div class="todo-item-content-description">
-        Go to forrest near the Zoo
+        {{description}}
       </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  // props: ['title', 'description'],
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: false,
+      default: 'Default Description'
+    }
+    // Array, Object, String, Number, Boolean, Function
+  }
+}
+</script>
 
 <style scoped lang="scss">
   .todo {
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,9 +2,15 @@
 
 <template>
   <div class="todo-list">
-    <todo-item />
-    <todo-item />
-    <todo-item />
+    <todo-item
+      v-bind:title="'Walk the dog'"
+      v-bind:description="'Go to forrest near the Zoo'" />
+    <todo-item
+      :title="'Buy a bread'"
+      :description="'Whole grain bread would be good'"/>
+    <todo-item
+      :title="todoTitle"
+      :description="todoDescription" />
   </div>
 </template>
 
@@ -13,6 +19,12 @@ import TodoItem from '@/components/TodoItem'
 export default {
   components: {
     TodoItem
+  },
+  data() {
+    return {
+      todoTitle: 'Learn Programming',
+      todoDescription: 'Preferable Tomorrow!'
+    }
   }
 }
 </script>
