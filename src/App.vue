0f6d0611b7824dcbe00674cf2376ec3af7a6bf44
Filip Jerga <jerga99@gmail.com>
2019-08-05 09:31:13 +0200

    33 Modal Separate Component

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,7 @@
         </div>
       </div>
     </div>
-    <div class="modal">
-      <div class="modal-content">
-        Modal Window
-      </div>
-    </div>
+    <Modal />
   </div>
 </template>
 
@@ -25,11 +21,12 @@
 <script>
 import playground from './playground'
 import TodoList from '@/components/TodoList'
+import Modal from '@/components/Modal'
 
 export default {
   name: 'app',
   components: {
-    TodoList
+    TodoList, Modal
   },
   data() {
     return {
@@ -85,28 +82,6 @@ $color-red: red;
     cursor: pointer;
   }
 }
-
-.modal {
-  display: none;
-  position: fixed; // stay in place
-  z-index: 9999;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  overflow: auto; // enable scroll if needed
-  background-color: rgba(0, 0, 0, 0.4);
-}
-
-.modal-content {
-  background-color: #fefefe;
-  margin: 15% auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-}
-
-
 
 .todo {
   &-wrapper {
---
+++ src/components/Modal.vue
@@ -0,0 +1,44 @@
+
+
+<template>
+  <div class="modal" :class="{'is-active': isActive}">
+    <div class="modal-content">
+      Modal Window
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isActive: true
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+
+  .modal {
+    display: none;
+    position: fixed; // stay in place
+    z-index: 9999;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto; // enable scroll if needed
+    background-color: rgba(0, 0, 0, 0.4);
+
+    &.is-active {
+      display: block;
+    }
+  }
+
+  .modal-content {
+    background-color: #fefefe;
+    margin: 15% auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+  }
+</style>
